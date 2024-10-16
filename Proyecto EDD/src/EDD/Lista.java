3f00bc064b36490107feb3d5608688a69d83daac
gabovizcarrondo <vizcarrondo.gabriel@correo.unimet.edu.ve>
2024-10-16 19:17:58 -0400

    Funciones1 clase lista

--- Proyecto EDD/src/EDD/Lista.java
+++ Proyecto EDD/src/EDD/Lista.java
@@ -4,6 +4,8 @@
  */
 package EDD;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author vizca
@@ -33,4 +35,66 @@ public class Lista {
         this.size = size;
     }
     
+    public boolean isEmpty(){
+        return pFirst == null;
+    }
+    public void insertBegin(Object dato){
+        Nodo pNew = new Nodo(dato);
+        if(this.isEmpty()){
+            this.setpFirst(pNew);
+        }else{
+            Nodo aux  = this.pFirst;
+            while(aux.getPnext() != null){
+                aux = aux.getPnext();
+        }
+        aux.setPnext(pNew);
+        }
+        size++;
+    }
+    public void deleteFinale(){
+        if(!this.isEmpty()){
+            if(size == 1) {
+               this.setpFirst(null);
+            }else{
+                Nodo aux = this.pFirst;
+                while (aux.getPnext().getPnext() != null) {
+                    aux = aux.getPnext();
+                }
+                aux.setPnext(null);
+            }
+            size--;
+        }
+    }
+    public boolean search (Object dato){
+        if (!this.isEmpty()){
+            if (size == 1){
+                return this.pFirst.getDato() == dato;  
+            }else{
+                Nodo aux = this.pFirst;
+                while (aux != null){
+                    if(aux.getDato() == dato){
+                        return true;
+                    }
+                    aux = aux.getPnext();
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+    public void print (){
+        if (!this.isEmpty()){
+            Nodo aux = this.pFirst;
+            String listaStr = "";
+            while(aux.getPnext() != null){
+                listaStr += aux.getDato()+ "\n";
+                aux = aux.getPnext();
+            }
+            listaStr += aux.getDato();
+            JOptionPane.showMessageDialog(null, listaStr);
+            
+        }else{
+            JOptionPane.showMessageDialog(null, "La lista esta vacia");
+        }
+    }
 }
