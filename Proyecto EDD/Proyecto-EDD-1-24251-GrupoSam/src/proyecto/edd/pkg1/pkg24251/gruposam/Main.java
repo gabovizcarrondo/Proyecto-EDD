3be51409567575811f761b362dcb283187d62b3f
FabianaObelmejias <fabiana.obelmejias@correo.unimet.edu.ve>
2024-10-21 00:41:27 -0400

    Clase Cola y funciones

---
+++ Proyecto EDD/Proyecto-EDD-1-24251-GrupoSam/src/proyecto/edd/pkg1/pkg24251/gruposam/Main.java
@@ -0,0 +1,78 @@
+
+import EDD.Cola;
+import EDD.Estacion;
+import EDD.Grafo;
+import EDD.Lista;
+import Interfaces.Inicio;
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Moises Liota
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Lista lista = new Lista();
+        
+//        lista.insertFinale(1);
+//        lista.deleteForReference(1);
+//        lista.insertFinale(2);
+//        lista.insertFinale(3);
+//        lista.insertFinale(4);
+//        
+//        lista.print();
+        
+        //lista.deleteForReference(4);
+        
+        //lista.print();
+          
+//        Cola cola = new Cola();
+//        cola.insert(1);
+//        cola.insert(2);
+//        cola.insert(3);
+//        
+//        cola.print();
+//        
+//        cola.delete();
+//        
+//        
+//        cola.print();
+
+//        Estacion estacion1 = new Estacion("Los Dos Caminos");
+//        Estacion estacion2 = new Estacion("Parque del Este");
+//        Estacion estacion3 = new Estacion("Altamira");
+//        
+//        Grafo grafo = new Grafo();
+//        
+//        grafo.insertStation("Los Dos Caminos");
+//        
+//        
+//        grafo.insertStation("Parque del Este");
+        //JOptionPane.showMessageDialog(null, grafo.toString());
+//        grafo.insertStation("Altamira");
+//        
+//        grafo.addConnection("Los Dos Caminos", "Parque del Este");
+//        grafo.addConnection("Parque del Este", "Altamira");
+        
+        //grafo.addConnection("Los Dos Caminos", "Parque del Este");
+        
+        //JOptionPane.showMessageDialog(null, grafo.toString());
+//        
+//        grafo.deleteConnection("Los Dos Caminos", "Parque del Este");
+//        
+//        
+//        JOptionPane.showMessageDialog(null, grafo.toString());
+        Inicio inicio = new Inicio();
+
+    }
+    
+}
